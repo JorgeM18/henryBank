84ce84a44fee8c25c61f60184666f4d4b5bfbdee
gonzalo <sundbladgonzalo@gmail.com>
2020-11-11 15:26:25 -0300

    mail templates con variable pin

--- api/controllers/users/users.controllers.js
+++ api/controllers/users/users.controllers.js
@@ -1,27 +1,23 @@
 
 const {User} = require('../../db');
 const bcrypt = require('bcrypt');
-
-
-const crypto = require('crypto');
+// const crypto = require('crypto');
 require('dotenv').config();
 const nodemailer = require('nodemailer');
-const { Sequelize } = require('sequelize');
-
-const Op = Sequelize.Op;
+// const { Sequelize } = require('sequelize');
+// var fs = require('fs');
+const hbs = require('nodemailer-express-handlebars');
+// const Op = Sequelize.Op;
 const BCRYPT_SALT_ROUNDS = 12;
 const {
     EMAIL_ADDRESS, 
     EMAIL_PASSWORD
   } = process.env;
 
-var fs = require('fs');
-
-
-var pin = Math.floor(Math.random() * 999999);
 
 
 const createUser =  async (ctx)=>{              // crea un usuario y envia el mail de validacion
+  var pin = Math.floor(Math.random() * 999999);
     ctx.params.pin = pin
     console.log(ctx.params)
   try{
@@ -37,49 +33,36 @@ const createUser =  async (ctx)=>{              // crea un usuario y envia el ma
         },
       });
 
-    //   const mailOptions = {
-    //     from: 'gohenrybank2020@gmail.com',
-    //     to: `${user.email}`,
-    //     subject: 'Gracias por ingresar! Confirme su cuenta',
-    //     text:
-    //       'Este es un mail para confirmar y completar su cuenta de Go Henry Bank\n\n'
-    //       + 'Por favor cliquea en el siguiente link, o copielo y peguelo en el navegador para completar el proceso.\n\n'
-    //       + `http://localhost:3000/NIDEA\n\n`
-    //       + 'Si usted no hizo este pedido, por favor ignore este mail.\n',
-    //   };
+    const options = {
+      viewEngine: {
+        partialsDir: __dirname + "/views/partials",
+        layoutsDir: './views/layouts', //ESTO ANDA MUY RARO. SOLO ME DEJA BUSCAR SI LA CARPETA VIEWS ESTA EN /API Y BUSCA COMO SI ESTUVIERA PARADO AHI (PONGO ../../ Y SALE DOS PARA ATRAS DE API. PONGO ./ Y LO ENCUENTRA) QCYOOO
+        extname: ".hbs"
+      },
+      extName: ".hbs",
+      viewPath: "views"
+    };
 
-    //   console.log('sending mail');
+    transporter.use('compile', hbs(options))
+    const pinObject = {}
+    pinObject.pin = pin
+   
+    const mailOptions = {
+      from: 'gohenrybank2020@gmail.com',
+      to: `${user.email}`,
+      subject: 'Gracias por ingresar! Confirme su cuenta',
+        template: "crateUserMail",
+        context: pinObject
 
-    //   transporter.sendMail(mailOptions, (err, response) => {
-    //     if (err) {
-    //       console.error('there was an error: ', err);
-    //       console.log('error en el envio de mail');
-    //     } else {
-    //       console.log('here is the res: ', response);
-    //       console.log('recovery email sent');
-    //     }
-    //   });
+    };
 
-    fs.readFile('./emailtemp/createUserTemp/crateUserMail.html', {encoding: 'utf-8'}, function (err, html) {
-        if (err) {
-          console.log(err);
+      transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+          console.log(error);
         } else {
-          var mailOptions = {
-            from: 'gohenrybank2020@gmail.com',
-            to: `${user.email}`,
-            subject: 'Gracias por ingresar! Confirme su cuenta',
-            html: html
-          };
-          transporter.sendMail(mailOptions, function(error, info) {
-            if (error) {
-              console.log(error);
-            } else {
-              console.log('Email sent: ' + info.response);
-            }
-          });
+          console.log('Email sent: ' + info.response);
         }
-    })
-
+      });
 
       const json = {
                message:"success", 
@@ -93,7 +76,6 @@ const createUser =  async (ctx)=>{              // crea un usuario y envia el ma
     }
 
 }
-
 
 
 const getMyData = async (ctx) => {  // obtener informacion del usuario segun id
@@ -113,16 +95,6 @@ const getMyData = async (ctx) => {  // obtener informacion del usuario segun id
         return 'noxo'
     }
 
-    // User.findOne({
-    //     where: {
-    //         id: ctx.params.id
-    //     }
-    // }).then(res => {
-    //     console.log(res)
-    //     return res              //POR Q NO ME LO MANDA AL POSTMAN? PERO SI LO CONSOLELOGUEA
-    // }).catch(err => {
-    //     return err
-    // })
 }
 
 
@@ -156,9 +128,6 @@ const editData = async (ctx) => {                         // editar num telefono
     }
 
 }
-
-
-
 
 
 module.exports = {
